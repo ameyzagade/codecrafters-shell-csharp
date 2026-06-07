95dc97b080ac8c80a79659ea562f18ad829ecfb2
Amey Zagade <ameyzagade1196@gmail.com>
2026-06-07 16:24:42 +0530

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -3,10 +3,12 @@ class Program
     static void Main()
     {
         // TODO: Uncomment the code below to pass the first stage
-        Console.Write("$ ");
-
-	string inputLine = Console.ReadLine();
-
-	Console.WriteLine($"{inputLine}: command not found");
+	while (true)
+	{
+        	Console.Write("$ ");
+		
+		string inputLine = Console.ReadLine();
+		Console.WriteLine($"{inputLine}: command not found");
+	}
     }
 }
