5c5a933e4f5a438891f32301d8955ba1ec395f5f
Amey Zagade <ameyzagade1196@gmail.com>
2026-06-07 16:18:45 +0530

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -4,5 +4,9 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+
+	string inputLine = Console.ReadLine();
+
+	Console.WriteLine($"{inputLine}: command not found");
     }
 }
